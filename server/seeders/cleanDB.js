25c56346bdd5d214876f3cf9094c84e6e61f77eb
Devunte <160948019+Devunte@users.noreply.github.com>
2024-06-09 16:57:21 -0500

    Create cleanDB.js

---
+++ server/seeders/cleanDB.js
@@ -0,0 +1,19 @@
+const models = require('../models');
+const db = require('../config/connection');
+
+const cleanDB = async (modelName, collectionName) => {
+  try {
+    let modelExists = await models[modelName].listCollections({
+      name: collectionName
+    }).toArray()
+
+    if (modelExists.length) {
+      await db.dropCollection(collectionName);
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
+
+module.exports = cleanDB;
